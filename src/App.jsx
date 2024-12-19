ac33486fbc8a526055f2f6deaac5a83936945fe7
Siddhant <onlyeeofficial@gmail.com>
2024-12-19 23:48:54 +0530

    Removed

--- src/App.jsx
+++ src/App.jsx
@@ -35,32 +35,3 @@ function App() {
   );
 }
 export default App;
-
-// export const Wallet: FC = () => {
-//   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-//   const network = WalletAdapterNetwork.Devnet;
-
-//   // You can also provide a custom RPC endpoint.
-//   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  
-//   const wallets = useMemo(
-//     () => [
-//       /**
-//        * Wallets that implement either of these standards will be available automatically.
-//        *
-//        *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-//        *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-//        *   - Solana Wallet Standard
-//        *     (https://github.com/anza-xyz/wallet-standard)
-//        *
-//        * If you wish to support a wallet that supports neither of those standards,
-//        * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-//        * in the npm package `@solana/wallet-adapter-wallets`.
-//        */
-//       new UnsafeBurnerWalletAdapter(),
-//     ],
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//     [network]
-//   );
-// };
-
